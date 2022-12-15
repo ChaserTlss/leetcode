6a0b9ea13353f373d3af2ae2ecc9b474f9b588ca
chaser.chen <tlss.g7chaser@gmail.com>
2022-12-15 14:18:27 +0800

    lc2: add two numbers
    fix carry count error.
    fix memory leak.

--- lc2.c
+++ lc2.c
@@ -1,46 +1,38 @@
- struct ListNode {
-     int val;
-     struct ListNode *next;
- };
- 
-#include <stdio.h>
-#include <stdlib.h>
+#include "lcHead.h"
 
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
+{
+	int carry = 0;
+	struct ListNode ret = {.val = 0, .next = NULL};
+	struct ListNode *t = &ret;
 
-struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
-    int carry = 0;
-    struct ListNode *t = NULL;
-    struct ListNode *ret = NULL;
+	while (l1 != NULL || l2 != NULL || carry != 0) {
+		t->next = malloc(sizeof(struct ListNode));
+		t = t->next;
+		t->next = NULL;
 
-    while (l1 != NULL || l2 != NULL || carry != 0) {
-        if (t) {
-            t->next = malloc(sizeof(struct ListNode));
-            /* if null */
-            t = t->next;
-        } else {
-            ret = t = malloc(sizeof(struct ListNode));
-        }
+		t->val = carry;
+		carry = 0;
+		if (l1 != NULL) {
+			t->val += l1->val;
+			carry = (t->val) / 10;
+			t->val = t->val % 10;
 
-        t->val = carry;
-        carry = 0;
-        if (l1 != NULL) {
-            t->val += l1->val;
-            carry += (l1->val + t->val) / 10;
-            t->val = t->val % 10;
-            l1 = l1->next;
-        }
+			l1 = l1->next;
+		}
 
-        if (l2 != NULL) {
-            t->val += l2->val;
-            carry += (l2->val + t->val) / 10;
-            t->val = t->val % 10;
-            l2 = l2->next;
-        }
-    }
-    return ret;
+		if (l2 != NULL) {
+			t->val += l2->val;
+			carry += (t->val) / 10;
+			t->val = t->val % 10;
+
+			l2 = l2->next;
+		}
+	}
+	return ret.next;
 }
 
-int main(void) {
+void case_1(void) {
 	struct ListNode l1m[3] = {
 		{2, &l1m[1]},
 		{4, &l1m[2]},
@@ -51,5 +43,14 @@ int main(void) {
 		{4, NULL}};
 
 	struct ListNode *t = addTwoNumbers(l1m, l2m);
-	return 0;
+
+	int a[] = {7, 0, 8};
+	listCheck(t, a, ARRAYSIZE(a));
+	while (t) {
+		struct ListNode *f = t;
+		t = t->next;
+		free(f);
+	}
 }
+
+REGISTER_TEST_CASE(case_1);
